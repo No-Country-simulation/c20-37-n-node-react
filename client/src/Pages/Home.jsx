acaf5da8eb6b85f7c7524bcd5a5d8896efce86ab
Leandro Martínez <leandro.martinez17@outlook.com>
2024-08-29 23:30:01 -0300

    fix: navbar

--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -5,7 +5,7 @@ export const Home = () => {
         <div className="flex flex-col justify-center items-center h-[calc(100vh-50px)]">
             <img className="sm:max-w-2xl" src="/Logo.png" alt="Logo SaludNet" />
             <div className="flex gap-x-4">
-                <Link to="/authentication">
+                <Link to={"/login"}>
                     <button className="bg-secondary hover:bg-teal-900 duration-300 text-white font-bold py-2 px-4 rounded-full">
                         Ingresar
                     </button>
--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -6,46 +6,38 @@ export const Header = () => {
     const [isMobileMenuOpen, setMobileMenuOpen] = useState(false);
     const toggleMobileMenu = () => {
         setMobileMenuOpen(!isMobileMenuOpen);
+        console.log(isMobileMenuOpen);
     };
     return (
-        <header className='fixed flex top-0 justify-center bg-secondary text-white w-full shadow-xl'>
-            <div className='flex max-w-6xl w-full'>
-                <div className='flex'>
-                    <Link to={'/'}>
-                        <img
-                            className='max-w-28 object-fill'
-                            src="/Logo.jpg" alt="SaludNet logo" />
-                    </Link>
-
-                </div>
-                <nav className='flex gap-x-4 w-full justify-between text-lg align-middle items-center font-semibold'>
-                    <ul className='hover:text-primary px-4 duration-200 transition-colors'><Link to={'/dashboard'}>Tablero</Link></ul>
-                </nav>
+        <nav className="flex top-0 fixed items-center justify-between flex-wrap bg-teal-500 w-full">
+            <div className="flex items-center flex-shrink-0 text-white">
+                <Link to={'/'}>
+                    <img
+                        className='max-w-16 lg:max-w-24 object-fill'
+                        src="/Logo.jpg" alt="SaludNet logo" />
+                </Link>
             </div>
-
-        </header>
-        // <header className='fixed flex top-0 bg-secondary text-white w-full shadow-xl'>
-        //     <nav className="flex items-center justify-between flex-wrap max-w-6xl">
-        //         <div className="flex items-center flex-shrink-0 text-white mr-6">
-        //             <Link to={'/'}>
-        //                 <img
-        //                     className='max-w-28 object-fill'
-        //                     src="/Logo.jpg" alt="SaludNet logo" />
-        //             </Link>
-        //         </div>
-        //         <div className="block md:hidden">
-        //             <button
-        //                 onClick={toggleMobileMenu}
-        //                 className="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white">
-        //                 <svg className="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" /></svg>
-        //             </button>
-        //         </div>
-        //         <div className={`w-full ${isMobileMenuOpen ? 'block' : 'hidden'} w-full block flex-grow lg:flex lg:items-center lg:w-auto `}>
-        //             <div className="text-sm lg:flex-grow">
-        //                  <ul className='hover:text-primary px-4 duration-200 transition-colors'><Link to={'/dashboard'}>Tablero</Link></ul>
-        //             </div>
-        //         </div>
-        //     </nav >
-        // </header>
+            <div
+                onClick={toggleMobileMenu}
+                className="block md:hidden">
+                <button className="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white">
+                    <svg className="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" /></svg>
+                </button>
+            </div>
+            <div
+                className={`w-full ${isMobileMenuOpen ? 'block' : 'hidden'} border border-t-white lg:border-none flex-grow lg:flex lg:items-center lg:w-auto  `}>
+                <div className="text-sm lg:flex-grow">
+                    <Link to={"/dashboard"} className="block mt-2 ml-2 lg:inline-block lg:mt-0 text-lg text-black hover:text-white mr-4">
+                        Dashboard
+                    </Link>
+                    <Link to={"#responsive-header"} className="block mt-4 ml-2 lg:inline-block lg:mt-0 text-lg text-black hover:text-white mr-4">
+                        Examples
+                    </Link>
+                    <Link to={"#responsive-header"} className="block mt-4 ml-2 lg:inline-block lg:mt-0 text-lg text-black hover:text-white">
+                        Blog
+                    </Link>
+                </div>
+            </div>
+        </nav >
     )
 }
