0a03f2c4ebc1ffdedff7751d2c8ff7d35d01fe0d
fernandasivila <fernandabsivila@gmail.com>
2024-09-01 01:45:23 -0300

    feat: diseña el header de la página

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,24 +1,47 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faRightToBracket, faUserPlus } from '@fortawesome/free-solid-svg-icons';
 import { Link } from 'react-router-dom';
 
 export const Header = () => {
     return (
-        <header className='fixed flex top-0 justify-center bg-secondary text-white w-full shadow-xl'>
-            <div className='flex justify-between max-w-6xl w-full'>
-                <div className='flex'>
-                    <Link to={'/dashboard'}>
-                        <img
-                            className='max-w-28 object-fill'
-                            src="/Logo.jpg" alt="SaludNet logo" />
+        <header className='font-poppins shadow-lg'>
+            <nav className="bg-background border-gray-200 px-4 lg:px-6 py-2.5">
+                <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
+                    <Link  to={'/dashboard'} className="flex items-center">
+                        <img src="/logotipo.png" alt="SaludNet Logo" className="mr-3 h-10 sm:h-12"/>
                     </Link>
-
+                    <div className="flex items-center lg:order-2">
+                        <Link to={'/login'} className="flex items-center text-primary bg-primary-700 hover:bg-muted focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2">
+                            <FontAwesomeIcon icon={faRightToBracket} className="text-primary" />
+                            <span className=" hidden sm:inline ml-2">Iniciar Sesión</span>
+                        </Link>
+                        <Link to={'/register'} className="flex items-center text-gray-800 hover:bg-primary hover:text-background transition-all duration-450 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2  focus:outline-none ">
+                            <FontAwesomeIcon icon={faUserPlus} />
+                            <span className="hidden sm:inline ml-2">Registrarse</span>
+                        </Link>
+                        <button data-collapse-toggle="mobile-menu" type="button" className="inline-flex items-center p-2 ml-1 text-sm text-gray-400 rounded-lg lg:hidden hover:bg-primary hover:text-background focus:outline-none focus:ring-2 focus:ring-gray-200" aria-controls="mobile-menu" aria-expanded="false">
+                            <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd"></path></svg>
+                            <svg className="hidden w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
+                        </button>
+                    </div>
+                    <div className="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1" id="mobile-menu">
+                        <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
+                            <li>
+                                <Link to={'/dashboard'} className="block py-2 pr-4 pl-3  text-primary border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-gray-400 lg:p-0">Inicio</Link>
+                            </li>
+                            <li>
+                                <Link to={'#'} className="block py-2 pr-4 pl-3 text-gray-400 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary lg:p-0">Clinica</Link>
+                            </li>
+                            <li>
+                                <Link to={'#'} className="block py-2 pr-4 pl-3 text-gray-400 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary lg:p-0">Especialistas</Link>
+                            </li>
+                            <li>
+                                <Link to={'#'} className="block py-2 pr-4 pl-3 text-gray-400 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary lg:p-0 ">Contacto</Link>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
-                <nav className='flex gap-x-4 justify-between text-lg align-middle items-center font-semibold'>
-                    {/* <ul className='hover:text-primary duration-200 transition-colors'><Link to={'/'}>Inicio</Link></ul> */}
-                    <ul className='hover:text-primary duration-200 transition-colors'><Link to={'/dashboard'}>Tablero</Link></ul>
-                    <ul className='hover:text-primary duration-200 transition-colors'><Link to={'/register'}>Registros</Link></ul>
-                </nav>
-            </div>
-
+            </nav>
         </header>
     )
 }
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -3,15 +3,29 @@ export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
+    'node_modules/flowbite-react/**/*.js'
   ],
   theme: {
     extend: {
       colors: {
-        'primary': '#00598e',
-        'secondary': '#0bb3a8',
+        /* 'primary': '#00598e',
+        'secondary': '#0bb3a8', */
+        'primary': '#0E588F',
+        'secondary': '#08A39F',
+        'background': '#FCFAF9',
+        'accent': '#5FB49C',
+        'text': '#07020D',
+        'muted': '#F1EFEF'
       },
+      fontFamily: {
+        lato: ['Lato'],
+        poppins: ['Poppins'],
+        bebas: ['Bebas Neue']
+      }
     },
   },
-  plugins: [],
+  plugins: [
+    require('flowbite/plugin')
+  ]
 }
 
