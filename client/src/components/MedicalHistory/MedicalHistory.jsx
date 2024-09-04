fa52ef1bdeef045c7ed27a63be08fc6572a207f7
Leandro Martínez <leandro.martinez17@outlook.com>
2024-09-04 13:09:30 -0300

    fix: medical history page

--- client/src/components/MedicalHistory/MedicalHistory.jsx
+++ client/src/components/MedicalHistory/MedicalHistory.jsx
@@ -6,7 +6,7 @@ export const MedicalHistory = () => {
 
     const { logued } = useAuth()
     const { medicalHistory, getMedicalHistoryById } = useUsers()
-    console.log(logued)
+    console.log(medicalHistory)
     const userMedicalInfo = {
         name: "Juan Pérez",
         age: "35",
@@ -25,23 +25,46 @@ export const MedicalHistory = () => {
         vaccines: "COVID-19 (2021), Vacuna contra la gripe (2022)",
         diagnosis: "Alergias estacionales"
     };
-    const getAllergies = () => {
-        const allergies = []
-        if (userMedicalInfo.allergies.food) allergies.push('Alimentos')
-        if (userMedicalInfo.allergies.medication) allergies.push('Medicamentos')
-        if (userMedicalInfo.allergies.environmental) allergies.push('Ambientales')
-        if (userMedicalInfo.allergies.other) allergies.push('Otros')
-        return allergies.length > 0 ? allergies.join(', ') : 'Ninguna'
+    const medicalHistorySchema = {
+        // medications: [{
+        //     type: String
+        // }],
+        medicalHistory: [{
+            type: String
+        }],
+        // allergies: [{
+        //     type: String
+        // }],
+        // vaccines: [{
+        //     type: String
+        // }],
+        consultations: [{
+            type: String
+        }],
+        // diagnoses: [{
+        //     type: String
+        // }],
+        attachedDocuments: [{
+            type: String
+        }],
     }
     // useEffect(() => {
     //     getMedicalHistoryById(logued.medicalHistory)
     // }, [])
     return (
-        <Card className="max-w-4xl mx-auto">
+        <Card className="max-w-4xl mx-auto roboto">
             <h2 className="text-2xl font-bold mb-4">Mi Información Médica</h2>
-            <button className='bg-black text-white' onClick={() => getMedicalHistoryById(logued.medicalHistory)}>Traer registro</button>
+            <button className='bg-black text-white' onClick={() => getMedicalHistoryById(logued.medicalHistory)}>Obtener Datos o Refrescar</button>
             <Table>
                 <Table.Body className="divide-y">
+                    <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
+                        <Table.Cell className="font-medium">Creación de ficha</Table.Cell>
+                        <Table.Cell>{medicalHistory.createdAt}</Table.Cell>
+                    </Table.Row>
+                    <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
+                        <Table.Cell className="font-medium">Última actualización</Table.Cell>
+                        <Table.Cell>{new Date(medicalHistory.updatedAt).toLocaleDateString()}</Table.Cell>
+                    </Table.Row>
                     <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
                         <Table.Cell className="font-medium">Nombre</Table.Cell>
                         <Table.Cell>{logued.firstName} {logued.lastName}</Table.Cell>
@@ -60,11 +83,15 @@ export const MedicalHistory = () => {
                     </Table.Row>
                     <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
                         <Table.Cell className="font-medium">Alergias</Table.Cell>
-                        <Table.Cell>
-                            <p>{getAllergies()}</p>
-                            {userMedicalInfo.allergyDetails && (
-                                <p className="mt-2 text-sm text-gray-600">{userMedicalInfo.allergyDetails}</p>
-                            )}
+                        <Table.Cell className='flex gap-x-1'>
+                            {medicalHistory.allergies ? medicalHistory.allergies.map((allergy, index) => (
+                                <div key={index} className='flex '>
+                                    <p color={'white'} size="lg" className="text-sm mr-2 px-1 border-gray-300 border">{allergy}</p>
+                                    {/* <button className='bg-red-500 px-1 text-white rounded-sm' onClick={() => console.log(allergy, index)} >x</button> */}
+                                </div>
+
+                            ))
+                                : 'No hay registros de alergias disponibles.'}
                         </Table.Cell>
                     </Table.Row>
                     <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
@@ -77,20 +104,41 @@ export const MedicalHistory = () => {
                     </Table.Row>
                     <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
                         <Table.Cell className="font-medium">Medicamentos Actuales</Table.Cell>
-                        <Table.Cell>{userMedicalInfo.medications || 'No se registran medicamentos actuales.'}</Table.Cell>
+                        <Table.Cell className='flex gap-x-1'>
+                            {medicalHistory.medications ? medicalHistory.medications.map((medication, index) => (
+                                <div key={index} className='flex'>
+                                    <p color={'white'} size="lg" className="text-sm mr-2 px-1 border-gray-300 border">{medication}</p>
+                                    {/* <button className='bg-red-500 px-1 text-white rounded-sm' onClick={() => console.log(allergy, index)} >x</button> */}
+                                </div>
+
+                            ))
+                                : 'No hay registros de medicaciones.'}
+                        </Table.Cell>
                     </Table.Row>
                     <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
                         <Table.Cell className="font-medium">Historial de Vacunación</Table.Cell>
-                        <Table.Cell>{userMedicalInfo.vaccines || 'No hay registros de vacunación disponibles.'}</Table.Cell>
+                        <Table.Cell className='flex gap-x-1'>
+                            {medicalHistory.vaccines ? medicalHistory.vaccines.map((vaccines, index) => (
+                                <div key={index} className='flex'>
+                                    <p color={'white'} size="lg" className="text-sm mr-2 px-1 border-gray-300 border">{vaccines}</p>
+                                    {/* <button className='bg-red-500 px-1 text-white rounded-sm' onClick={() => console.log(allergy, index)} >x</button> */}
+                                </div>
+
+                            ))
+                                : 'No hay registros de vacunas.'}
+                        </Table.Cell>
                     </Table.Row>
                     <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
                         <Table.Cell className="font-medium">Diagnóstico Actual</Table.Cell>
-                        <Table.Cell>
-                            {userMedicalInfo.diagnosis ? (
-                                <Badge color="warning" size="lg">{userMedicalInfo.diagnosis}</Badge>
-                            ) : (
-                                'No hay diagnóstico actual.'
-                            )}
+                        <Table.Cell className='flex gap-x-1'>
+                            {medicalHistory.diagnoses ? medicalHistory.vaccines.map((diagnoses, index) => (
+                                <div key={index} className='flex'>
+                                    <p color={'white'} size="lg" className="text-sm mr-2 px-1 border-gray-300 border">{diagnoses}</p>
+                                    {/* <button className='bg-red-500 px-1 text-white rounded-sm' onClick={() => console.log(allergy, index)} >x</button> */}
+                                </div>
+
+                            ))
+                                : 'No hay registros de diagnosticos.'}
                         </Table.Cell>
                     </Table.Row>
                 </Table.Body>
