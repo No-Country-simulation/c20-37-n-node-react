305193f5aa01830d3737492c401a12226dd0ecc6
ffffacu <ffffacu@hotmail.com>
2024-09-03 22:20:48 -0300

    feat/backendMedicalHistoryReform

--- backend/src/modules/medicalHistory/medicalHistory.model.js
+++ backend/src/modules/medicalHistory/medicalHistory.model.js
@@ -2,65 +2,29 @@ import mongoose from 'mongoose';
 
 const medicalHistorySessionCollection = "medicalHistory";
 
-const medicalHistorySchema = new mongoose.Schema({
-    medications: {
-        type: [{
-            name: String,
-            dosage: String,
-            startDate: Date,
-            endDate: Date
-        }],
-        default: []
-    },
-    medicalHistory: {
-        type: [{
-            condition: String,
-            diagnosisDate: Date,
-            treatment: String
-        }],
-        default: []
-    },
-    allergies: {
-        type: [{
-            allergen: String,
-            reaction: String,
-            severity: String
-        }],
-        default: []
-    },
-    vaccines: {
-        type: [{
-            name: String,
-            administrationDate: Date,
-            dosage: String
-        }],
-        default: []
-    },
-    consultations: {
-        type: [{
-            consultationDate: Date,
-            doctor: String,
-            notes: String
-        }],
-        default: []
-    },
-    diagnoses: {
-        type: [{
-            diagnosis: String,
-            date: Date,
-            treatment: String
-        }],
-        default: []
-    },
-    attachedDocuments: {
-        type: [{
-            name: String,
-            url: String,
-            uploadDate: Date
-        }],
-        default: []
-    }
-}, {
+const medicalHistorySchema = new mongoose.Schema([{
+    medications: [{
+        type: String
+    }],
+    medicalHistory: [{
+        type: String
+    }],
+    allergies:[ {
+        type: String
+    }],
+    vaccines: [{
+        type: String
+    }],
+    consultations:[ {
+        type: String
+    }],
+    diagnoses: [{
+        type: String
+    }],
+    attachedDocuments: [{
+        type: String
+    }],
+}], {
     timestamps: true
 });
 export const medicalHistoryModel = mongoose.model(medicalHistorySessionCollection, medicalHistorySchema);
