dc7f36c24489b03277aed6603bf529d53c020a3c
Leandro Martínez <leandro.martinez17@outlook.com>
2024-09-06 01:32:35 -0300

    feat: frontend url env

--- backend/src/app.js
+++ backend/src/app.js
@@ -26,7 +26,7 @@ app.use(session(
     }
 ));
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: envs.FRONTEND_URL,
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization']
