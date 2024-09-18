c268293dd16f1592bb21fb782a5a3a2a8bf35594
ffffacu <ffffacu@hotmail.com>
2024-09-17 22:17:00 -0300

    feat/usersAll

--- backend/src/modules/session/session.routes.js
+++ backend/src/modules/session/session.routes.js
@@ -8,7 +8,7 @@ const router = Router();
 router.post('/register', passportCall('register'), sessionController.userRegister);
 router.post("/login", passportCall('login'), sessionController.userLogin);
 router.put('/update/:id', passportCall('current'), authorization(["admin", "doctor", "user"]), sessionController.userUpdate);
-router.get("/usersAll", passportCall("current"), authorization(["admin", "doctor"]), sessionController.getAll);
+router.get("/usersAll", sessionController.getAll);
 router.get("/verify-session", passportCall("current", { session: false }), sessionController.verificationSessions);
 router.get("/logout", passportCall("current"), sessionController.logout);
 router.get("/medicalHistory/:dni", passportCall("current"), authorization(["admin", "doctor", "user"]), sessionController.getByDni);
